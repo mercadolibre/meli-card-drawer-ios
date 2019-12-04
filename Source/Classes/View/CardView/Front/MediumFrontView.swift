5846ac66d5958878729afac5e7296ca787b7aff7
JONATHAN DANIEL BANDONI <jonathan.bandoni@mercadolibre.com>
2019-12-04 13:14:38 -0300

    mejoras propuestas

--- Source/Classes/View/CardView/Front/MediumFrontView.swift
+++ Source/Classes/View/CardView/Front/MediumFrontView.swift
@@ -8,7 +8,7 @@ class MediumFrontView: CardView {
     @IBOutlet weak var nameLabel: CardLabel!
     @IBOutlet weak var chevronIcon: UIImageView!
     
-    @IBOutlet weak var chevronTrailingConstraint: NSLayoutConstraint!
+    @IBOutlet weak var numberTrailingConstraint: NSLayoutConstraint!
     
     private var shineView: ShineView?
 
@@ -51,13 +51,15 @@ extension MediumFrontView {
     }
     
     private func setupChevron(_ cardUI: CardUI) {
-        let showChevron = (cardUI.showChevron == true)
-        
-        chevronTrailingConstraint.constant = showChevron ? -14 : 2
-        chevronIcon.isHidden = !showChevron
+        showChevron(cardUI.showChevron == true)
         
         chevronIcon.image = chevronIcon.image?.withRenderingMode(.alwaysTemplate)
         chevronIcon.tintColor = nameLabel.typeFont.gradient.getGradient(frame)
+    }
+    
+    private func showChevron(_ value: Bool) {
+        numberTrailingConstraint.priority = value ? .required : .defaultLow
+        chevronIcon.isHidden = !value
     }
     
     private func setupFormatters(_ cardUI: CardUI) {
