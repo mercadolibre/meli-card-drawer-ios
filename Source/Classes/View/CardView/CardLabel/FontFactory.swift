2b07b18cbd969e029a2adcdcd156b5669305bbc3
Esteban Adrian Boffa <esteban.boffa@mercadolibre.com>
2020-03-06 16:59:06 -0300

    Update font color and remove gradient

--- Source/Classes/View/CardView/CardLabel/FontFactory.swift
+++ Source/Classes/View/CardView/CardLabel/FontFactory.swift
@@ -38,8 +38,8 @@ struct Default: Font {
 }
 
 struct Light: Font {
-    var color: UIColor = UIColor(red: 255/255, green: 255/255, blue: 255/255, alpha: 0.8)
-    var gradient: Gradient = Gradient(top: UIColor(red: 224/255, green: 224/255, blue: 224/255, alpha: 1),
+    var color: UIColor = UIColor(red: 255/255, green: 255/255, blue: 255/255, alpha: 1)
+    var gradient: Gradient = Gradient(top: UIColor(red: 255/255, green: 255/255, blue: 255/255, alpha: 1),
                                       bottom: UIColor(red: 255/255, green: 255/255, blue: 255/255, alpha: 1))
     var shadow: Bool
 
@@ -49,9 +49,9 @@ struct Light: Font {
 }
 
 struct Dark: Font {
-    var color: UIColor = UIColor(red: 102/255, green: 102/255, blue: 102/255, alpha: 1)
-    var gradient: Gradient = Gradient(top: UIColor(red: 119/255, green: 119/255, blue: 119/255, alpha: 1),
-                                      bottom: UIColor(red: 85/255, green: 85/255, blue: 85/255, alpha: 1))
+    var color: UIColor = UIColor(red: 0/255, green: 0/255, blue: 0/255, alpha: 0.8)
+    var gradient: Gradient = Gradient(top: UIColor(red: 0/255, green: 0/255, blue: 0/255, alpha: 0.8),
+                                      bottom: UIColor(red: 0/255, green: 0/255, blue: 0/255, alpha: 0.8))
     var shadow: Bool
 
     init(shadow: Bool) {
