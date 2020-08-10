403bf41a60e3200c7aca9fcf3dc9734a1fe3b19d
lelalesi <alexis.garcia@mercadolibre.com>
2020-08-10 17:10:43 -0300

    front view changes

--- Source/Classes/View/CardView/Front/FrontView.swift
+++ Source/Classes/View/CardView/Front/FrontView.swift
@@ -3,8 +3,6 @@ import UIKit
 class FrontView: CardView {
     @IBOutlet weak var expirationDate: CardLabel!
     @IBOutlet weak var name: CardLabel!
-    @IBOutlet weak var logo: UIImageView!
-    @IBOutlet weak var bank: UIImageView!
     @IBOutlet weak var remotePaymentMethodImage: UIImageView!
     @IBOutlet weak var remoteBankImage: UIImageView!
     @IBOutlet weak var number: CardLabel!
@@ -16,10 +14,10 @@ class FrontView: CardView {
         
         setupSecurityCode(cardUI)
         if cardUI.set(logo:) != nil {
-            setupCardLogo(in: logo)
+            setupCardLogo(in: remotePaymentMethodImage)
         }
         if cardUI.set(bank:) != nil {
-            setupBankImage(in: bank)
+            setupBankImage(in: remoteBankImage)
         }
         setupRemoteOrLocalImages(cardUI)
         
@@ -73,7 +71,7 @@ extension FrontView {
         if !(cardUI is CustomCardDrawerUI) {
             Animator.overlay(on: self,
                              cardUI: cardUI,
-                             views: [bank, expirationDate, logo, name, number, securityCode],
+                             views: [remoteBankImage, expirationDate, remotePaymentMethodImage, name, number, securityCode],
                              complete: {[weak self] in
                                 self?.setupUI(cardUI)
             })
@@ -90,7 +88,6 @@ extension FrontView {
     }
 
     private func setPaymentMethodImage(_ cardUI: CardUI) {
-        logo.image = nil
         remotePaymentMethodImage.image = nil
         if let imageUrl = cardUI.cardLogoImageUrl as? String, !imageUrl.isEmpty {
             UIImageView().getRemoteImage(imageUrl: imageUrl) { image in
@@ -100,12 +97,11 @@ extension FrontView {
                 }
             }
         } else if let image = cardUI.cardLogoImage as? UIImage {
-            setImage(image, inImageView: logo)
+            setImage(image, inImageView: remotePaymentMethodImage, scaleHeight: true)
         }
     }
 
     private func setBankImage(_ cardUI: CardUI) {
-        bank.image = nil
         remoteBankImage.image = nil
         if let imageUrl = cardUI.bankImageUrl as? String, !imageUrl.isEmpty {
             UIImageView().getRemoteImage(imageUrl: imageUrl) { remoteBankImage in
@@ -115,10 +111,8 @@ extension FrontView {
                 }
             }
         } else if let image = cardUI.bankImage as? UIImage {
-            setImage(image, inImageView: bank)
+            setImage(image, inImageView: remoteBankImage, scaleHeight: true)
         }
-        
-        setupBankImage(in: bank)
     }
 
     private func setImage(_ tImage: UIImage, inImageView: UIImageView, scaleHeight: Bool = false) {
