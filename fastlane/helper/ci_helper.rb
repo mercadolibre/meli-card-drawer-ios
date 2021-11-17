9311bc220ca5bd993dee71a839c944a2578d1551
Jona <jonathan.scaramal@mercadolibre.com>
2021-11-17 14:56:27 -0300

    chore: Debug

--- fastlane/helper/ci_helper.rb
+++ fastlane/helper/ci_helper.rb
@@ -4,18 +4,18 @@ module CIHelper
 
     if ENV['PX_BITRISE_CI']
         LIB_NAME = ENV['PX_BITRISE_LIB_NAME']
-        BASE_BRANCH = ENV['BITRISEIO_PULL_REQUEST_MERGE_BRANCH']
+        MERGE_BRANCH = ENV['BITRISE_GIT_BRANCH']
         BUILD_DIR = ENV['BITRISE_SOURCE_DIR']
         SCHEME = ENV['PX_BITRISE_SCHEME']
         TEST_SCHEME = ENV['PX_BITRISE_TEST_SCHEME']
     end
 
     def self.is_release_branch?
-        return CIHelper::BASE_BRANCH.match?(/^release\/([0-9])+\.([0-9])+\.([0-9])+$/)
+        return CIHelper::MERGE_BRANCH.match?(/^release\/([0-9])+\.([0-9])+\.([0-9])+$/)
     end
 
     def self.get_lib_version
-        return CIHelper::BASE_BRANCH.match(/release\/(.*)/)[1]
+        return CIHelper::MERGE_BRANCH.match(/release\/(.*)/)[1]
     end
 
     def self.get_spec_version
