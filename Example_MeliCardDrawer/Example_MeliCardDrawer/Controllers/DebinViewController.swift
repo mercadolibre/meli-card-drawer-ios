5b549a2515cefc03312249cced6de362438039d0
Gisela Ramos <gisela.ramos@mercadolibre.com>
2022-01-19 17:03:03 -0300

    PXN-2643 Add Debin option in app example

---
+++ Example_MeliCardDrawer/Example_MeliCardDrawer/Controllers/DebinViewController.swift
@@ -0,0 +1,62 @@
+//
+//  DebinViewController.swift
+//  Example_MeliCardDrawer
+//
+//  Created by Gisela Araceli Ramos Carrasco on 19/01/2022.
+//  Copyright © 2022 Mercadolibre. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import MLCardDrawer
+
+class DebinViewController: UIViewController {
+    @IBOutlet weak var containerView: UIView!
+    @IBOutlet weak var segmentedControl: UISegmentedControl!
+    @IBOutlet weak var containerViewHeightConstraint: NSLayoutConstraint!
+    
+    var cardType: MLCardDrawerTypeV3 = .large
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        segmentedControl.addTarget(nil, action: #selector(valueChanged), for: .valueChanged)
+        setupCard(cardType: cardType)
+    }
+    
+    @objc func valueChanged() {
+        switch segmentedControl.selectedSegmentIndex {
+        case 0:
+            cardType = .large
+            containerViewHeightConstraint.constant = 250
+            
+        case 1:
+            cardType = .small
+            containerViewHeightConstraint.constant = 105
+            
+        default:
+            break
+        }
+        
+        setupCard(cardType: cardType)
+    }
+    
+    private func setupCard(cardType: MLCardDrawerTypeV3) {
+        containerView.subviews.forEach { $0.removeFromSuperview() }
+        
+        let cardDrawer = MLCardDrawerController(cardUI: CardUIExamples.Debin(), cardType, CardDataHandler())
+                
+        let cardView = cardDrawer.getCardView()
+        
+        cardView.translatesAutoresizingMaskIntoConstraints = false
+        containerView.layer.masksToBounds = true
+        
+        containerView.addSubview(cardView)
+        
+        NSLayoutConstraint.activate([
+            cardView.topAnchor.constraint(equalTo: containerView.topAnchor),
+            cardView.leadingAnchor.constraint(equalTo: containerView.leadingAnchor),
+            cardView.bottomAnchor.constraint(equalTo: containerView.bottomAnchor),
+            cardView.trailingAnchor.constraint(equalTo: containerView.trailingAnchor)])
+    }
+
+}
