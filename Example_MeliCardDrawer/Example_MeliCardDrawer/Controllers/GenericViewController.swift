e994ed68e8531fe7b72cc450875d99c68b550a80
Gisela Ramos <gisela.ramos@mercadolibre.com>
2022-01-19 17:03:50 -0300

    PXN-2643 Add gradient option for generic card

--- Example_MeliCardDrawer/Example_MeliCardDrawer/Controllers/GenericViewController.swift
+++ Example_MeliCardDrawer/Example_MeliCardDrawer/Controllers/GenericViewController.swift
@@ -11,13 +11,49 @@ import MLCardDrawer
 
 class GenericViewController: UIViewController {
 
-    @IBOutlet var containerView: UIView!
+    @IBOutlet weak var containerView: UIView!
+    @IBOutlet weak var segmentedControl: UISegmentedControl!
+    @IBOutlet weak var containerViewHeightConstraint: NSLayoutConstraint!
+    @IBOutlet weak var gradientColorsSwitch: UISwitch!
+    
+    var cardType: MLCardDrawerTypeV3 = .large
+    var cardTemplate = CardUIExamples.Pix()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        gradientColorsSwitch.addTarget(nil, action: #selector(enabledGradientColors), for: .valueChanged)
+        segmentedControl.addTarget(nil, action: #selector(changedValue), for: .valueChanged)
+        setupCard(cardType: cardType)
+    }
+    
+    @objc func enabledGradientColors() {
+        cardTemplate.gradientColors = gradientColorsSwitch.isOn ? ["#132F3B", "#3688AB", "#37B4AA"] : [""]
+        cardTemplate.labelBackgroundColor = gradientColorsSwitch.isOn ? "#FFFFFF" : "#1A479AD1"
+        setupCard(cardType: cardType)
+    }
+    
+    @objc func changedValue() {
+        switch segmentedControl.selectedSegmentIndex {
+        case 0:
+            cardType = .large
+            containerViewHeightConstraint.constant = 250
+            
+        case 1:
+            cardType = .small
+            containerViewHeightConstraint.constant = 105
+            
+        default:
+            break
+        }
         
-        let cardDrawer = MLCardDrawerController(CardUIExamples.Pix(), CardDataHandler())
+        setupCard(cardType: cardType)
+    }
+
+    private func setupCard(cardType: MLCardDrawerTypeV3) {
+        containerView.subviews.forEach { $0.removeFromSuperview() }
         
+        let cardDrawer = MLCardDrawerController(cardUI: cardTemplate, cardType, CardDataHandler())
+                
         let cardView = cardDrawer.getCardView()
         
         cardView.translatesAutoresizingMaskIntoConstraints = false
@@ -27,23 +63,14 @@ class GenericViewController: UIViewController {
         
         cardView.layer.borderColor = UIColor.black.cgColor
         cardView.layer.borderWidth = 1
+        cardView.layer.cornerRadius = 10
         
-        NSLayoutConstraint.activate([cardView.topAnchor.constraint(equalTo: containerView.topAnchor),
-                                     cardView.leadingAnchor.constraint(equalTo: containerView.leadingAnchor),
-                                     cardView.bottomAnchor.constraint(equalTo: containerView.bottomAnchor),
-                                     cardView.trailingAnchor.constraint(equalTo: containerView.trailingAnchor)])
-        
+        NSLayoutConstraint.activate([
+            cardView.topAnchor.constraint(equalTo: containerView.topAnchor),
+            cardView.leadingAnchor.constraint(equalTo: containerView.leadingAnchor),
+            cardView.bottomAnchor.constraint(equalTo: containerView.bottomAnchor),
+            cardView.trailingAnchor.constraint(equalTo: containerView.trailingAnchor)
+        ])
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
