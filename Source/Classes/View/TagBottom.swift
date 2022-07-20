2c45c0d0103eec15b5ab07e581ff3b49a49b91df
matheusctMeli <matheus.mteixeira@mercadolivre.com>
2022-07-20 09:43:42 -0300

    Bugfix/pxn 4523 error bezierpath tagbottom (#102)
    * fiz error in tagbottom bezierpath when card with tagbottom is removed from screen
    
    * changelog
    
    * update resize diference

--- Source/Classes/View/TagBottom.swift
+++ Source/Classes/View/TagBottom.swift
@@ -31,11 +31,22 @@ public class TagBottom: UILabel {
         }
     }
     
+    public override func sizeThatFits(_ size: CGSize) -> CGSize {
+        return addInsets(to: super.sizeThatFits(size))
+    }
+
+    
     public override func didMoveToWindow() {
         let path = UIBezierPath(roundedRect: self.bounds, byRoundingCorners: [.topLeft, .bottomLeft], cornerRadii: CGSize(width: self.bounds.height/2, height: self.bounds.height/2))
         let mask = CAShapeLayer()
         mask.path = path.cgPath
         self.layer.mask = mask
+    }
+        
+    private func addInsets(to size: CGSize) -> CGSize {
+        let width = size.width + leftInset + rightInset
+        let height = size.height + topInset + bottomInset - 2
+        return CGSize(width: width, height: height)
     }
     
 }
