aa43eaf3ea4d22f3c3b5c800ba1d7cd0df704f56
Matheus Cavalcante Teixeira <matheus.mteixeira@mercadolivre.com>
2022-08-05 17:33:39 -0300

    fix tagbottom text space

--- Source/Classes/View/TagBottom.swift
+++ Source/Classes/View/TagBottom.swift
@@ -9,10 +9,10 @@ import UIKit
 
 public class TagBottom: UILabel {
     
-    let topInset: CGFloat = 7.0
-    let bottomInset: CGFloat = 5.0
+    let topInset: CGFloat = 8.0
+    let bottomInset: CGFloat = 8.0
     let leftInset: CGFloat = 8.0
-    let rightInset: CGFloat = 8.0
+    let rightInset: CGFloat = 9.0
       
     public override func drawText(in rect: CGRect) {
         let insets = UIEdgeInsets(top: topInset, left: leftInset, bottom: bottomInset, right: rightInset)
@@ -35,17 +35,18 @@ public class TagBottom: UILabel {
         return addInsets(to: super.sizeThatFits(size))
     }
 
-    
-    public override func didMoveToWindow() {
-        let path = UIBezierPath(roundedRect: self.bounds, byRoundingCorners: [.topLeft, .bottomLeft], cornerRadii: CGSize(width: self.bounds.height/2, height: self.bounds.height/2))
+    public override func draw(_ rect: CGRect) {
+        let path = UIBezierPath(roundedRect: rect, byRoundingCorners: [.topLeft, .bottomLeft], cornerRadii: CGSize(width: self.bounds.height/2, height: self.bounds.height/2))
         let mask = CAShapeLayer()
         mask.path = path.cgPath
         self.layer.mask = mask
+        drawText(in: rect)
     }
+
         
     private func addInsets(to size: CGSize) -> CGSize {
         let width = size.width + leftInset + rightInset
-        let height = size.height + topInset + bottomInset - 2
+        let height = size.height + topInset + bottomInset - 6
         return CGSize(width: width, height: height)
     }
     
