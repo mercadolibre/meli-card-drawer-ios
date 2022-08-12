fc222f54e0564dd2a8195dd5ca19881c390c9b23
Marcos Varani <marcos.varani@mercadolibre.com>
2022-08-12 12:26:22 -0300

    Refactored PAN UI constants

--- Source/Classes/View/PAN/PANView.swift
+++ Source/Classes/View/PAN/PANView.swift
@@ -1,17 +1,22 @@
 import UIKit
 
 class PANView: UIView {
+   
+    enum PANLabelUI {
+        static let labelTextColor: UIColor = .white
+        static let labelBackgroundColor: UIColor = .clear
+        static let labelFontSize: CGFloat = 14.0
+        static let labelPlaceholder: String = "•••• ••••"
+        static let topPadding: CGFloat = 4.0
+        static let bottomPadding: CGFloat = -4.0
+        static let leftPadding: CGFloat = 8.0
+        static let rightPadding: CGFloat = -8.0
+    }
     
-    private let labelTextColor: UIColor = .white
-    private let labelBackgroundColor: UIColor = .clear
-    private let labelFontSize: CGFloat = 14.0
-    private let labelPlaceholder: String = "•••• ••••"
-    private let containerBackgroundColor: UIColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.55)
-    private let containerCornerRadius: CGFloat = 4.0
-    private let topPadding: CGFloat = 4.0
-    private let bottomPadding: CGFloat = -4.0
-    private let leftPadding: CGFloat = 8.0
-    private let rightPadding: CGFloat = -8.0
+    enum PANContainerUI {
+        static let containerBackgroundColor: UIColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.55)
+        static let containerCornerRadius: CGFloat = 4.0
+    }
 
     private var PANLabel: UILabel!
     private var PANContainer: UIView!
@@ -36,18 +41,18 @@ class PANView: UIView {
     
     private func setupLabel() {
         PANLabel = UILabel()
-        PANLabel.font = UIFont.systemFont(ofSize: labelFontSize, weight: .semibold)
-        PANLabel.text = labelPlaceholder
-        PANLabel.backgroundColor = labelBackgroundColor
-        PANLabel.textColor = labelTextColor
+        PANLabel.font = UIFont.systemFont(ofSize: PANLabelUI.labelFontSize, weight: .semibold)
+        PANLabel.text = PANLabelUI.labelPlaceholder
+        PANLabel.backgroundColor = PANLabelUI.labelBackgroundColor
+        PANLabel.textColor = PANLabelUI.labelTextColor
         PANLabel.clipsToBounds = true
         PANLabel.sizeToFit()
     }
     
     private func setupContainer() {
         PANContainer = UIView()
-        PANContainer.backgroundColor = containerBackgroundColor
-        PANContainer.layer.cornerRadius = containerCornerRadius
+        PANContainer.backgroundColor = PANContainerUI.containerBackgroundColor
+        PANContainer.layer.cornerRadius = PANContainerUI.containerCornerRadius
         PANContainer.clipsToBounds = true
         PANContainer.sizeToFit()
     }
@@ -64,10 +69,10 @@ class PANView: UIView {
             PANContainer.bottomAnchor.constraint(equalTo: self.bottomAnchor),
             PANContainer.rightAnchor.constraint(equalTo: self.rightAnchor),
             PANContainer.leftAnchor.constraint(equalTo: self.leftAnchor),
-            PANLabel.topAnchor.constraint(equalTo: PANContainer.topAnchor, constant: topPadding),
-            PANLabel.bottomAnchor.constraint(equalTo: PANContainer.bottomAnchor, constant: bottomPadding),
-            PANLabel.rightAnchor.constraint(equalTo: PANContainer.rightAnchor, constant: rightPadding),
-            PANLabel.leftAnchor.constraint(equalTo: PANContainer.leftAnchor, constant: leftPadding)
+            PANLabel.topAnchor.constraint(equalTo: PANContainer.topAnchor, constant: PANLabelUI.topPadding),
+            PANLabel.bottomAnchor.constraint(equalTo: PANContainer.bottomAnchor, constant: PANLabelUI.bottomPadding),
+            PANLabel.rightAnchor.constraint(equalTo: PANContainer.rightAnchor, constant: PANLabelUI.rightPadding),
+            PANLabel.leftAnchor.constraint(equalTo: PANContainer.leftAnchor, constant: PANLabelUI.leftPadding)
         ])
     }
 }
@@ -102,6 +107,6 @@ extension PANView {
     }
     
     private func setTextWeight(_ textWeight: String) {
-        PANLabel.font = textWeight.getFont(size: labelFontSize)
+        PANLabel.font = textWeight.getFont(size: PANLabelUI.labelFontSize)
     }
 }
