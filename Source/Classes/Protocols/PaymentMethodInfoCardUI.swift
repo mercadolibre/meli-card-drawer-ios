09b91ff6f4b725e678b2f3c95d0cc0e086aa0fca
Marcos Varani <marcos.varani@mercadolibre.com>
2022-09-29 16:17:19 -0300

    Added specific cardUI for our new PaymentMethodInfo Cards

--- Source/Classes/Protocols/PaymentMethodInfoCardUI.swift
+++ Source/Classes/Protocols/PaymentMethodInfoCardUI.swift
@@ -1,16 +1,31 @@
 import Foundation
 
 @objc public protocol PaymentMethodInfoCardUI: CardUI {
-    var topLeftContentText: String? { get }
-    var topLeftContentTextColor: String? { get }
-    var topLeftContentTextWeight: String? { get }
-    var topLeftContentTextAlignment: String? { get }
-    var topRightContentText: String? { get }
-    var topRightContentTextColor: String? { get }
-    var topRightContentTextWeight: String? { get }
-    var topRightContentTextAlignment: String? { get }
-    var bottomRightContentText: String? { get }
-    var bottomRightContentTextColor: String? { get }
-    var bottomRightContentTextWeight: String? { get }
-    var bottomRightContentTextAlignment: String? { get }
+    @objc optional var entity: CustomText { get }
+    @objc optional var amount: CustomText { get }
+    @objc optional var paymentType: CustomText { get }
+    @objc optional var gradient: CAGradientLayer { get }
+    @objc optional var overlay: UIImage? { get }
+}
+
+@objc public class CustomText: NSObject {
+    public var message: String?
+    public var backgroundColor: UIColor?
+    public var textColor: UIColor?
+    public var weight: String?
+    public var alignment: NSTextAlignment?
+    
+    public init(
+        message: String?,
+        backgroundColor: UIColor?,
+        textColor: UIColor?,
+        weight: String?,
+        alignment: NSTextAlignment?
+    ) {
+        self.message = message
+        self.backgroundColor = backgroundColor
+        self.textColor = textColor
+        self.weight = weight
+        self.alignment = alignment
+    }
 }
