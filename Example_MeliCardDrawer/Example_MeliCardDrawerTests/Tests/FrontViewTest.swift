fbce93d08f8a4e1c69a731aaf737e578a5c35850
Marcos Varani <marcos.varani@mercadolibre.com>
2022-11-28 12:32:56 -0300

    Removed eliminated properties from FrontView test

--- Example_MeliCardDrawer/Example_MeliCardDrawerTests/Tests/FrontViewTest.swift
+++ Example_MeliCardDrawer/Example_MeliCardDrawerTests/Tests/FrontViewTest.swift
@@ -10,29 +10,11 @@ class FrontViewTest: XCTestCase {
         let frontView = FrontView()
 
         frontView.setup(cardUI, model, .zero)
-        frontView.model?.name = "JOHN"
-        frontView.model?.number = "123"
         frontView.model?.securityCode = "1"
-        frontView.model?.expiration = "10/19"
-        
+
         XCTAssertNotNil(frontView.model)
-        XCTAssert(frontView.name.text! == "JOHN")
-        XCTAssert(frontView.number.text! == "123*   ****   ****")
         XCTAssert(frontView.securityCode.text! == "1**")
-        XCTAssert(frontView.expirationDate.text! == "10/19")
         XCTAssert(frontView.securityCode.alpha == 1)
-    }
-
-    func testCardUIDidChange() {
-        let cardUI = CardUIMock()
-        let model = CardDataMock()
-        let frontView = FrontView()
-
-        frontView.setup(cardUI, model, .zero)
-        cardUI.cardPattern = [3, 3]
-        frontView.setupUI(cardUI)
-
-        XCTAssert(frontView.number.text! == "***             ***")
     }
 
     func testShowCVV() {
